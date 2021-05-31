0e48f62da09f1df5047b4ed75e4884baa8c4c936
Kenny Pflug <kenny.pflug@live.de>
2021-05-31 07:23:33 +0200

    added Linq2Db.MsSqlServer projects

---
+++ Code/src/Synnotech.Linq2Db.MsSqlServer/DataConnectionExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Light.GuardClauses;
+using Light.GuardClauses.Exceptions;
+using LinqToDB.Data;
+using LinqToDB.DataProvider.SqlServer;
+using Microsoft.Data.SqlClient;
+
+namespace Synnotech.Linq2Db.MsSqlServer
+{
+    /// <summary>
+    /// Provides extension methods for <see cref="DataConnection" />.
+    /// </summary>
+    public static class DataConnectionExtensions
+    {
+        /// <summary>
+        /// Creates a <see cref="SqlCommand" /> from the DB connection that is associated
+        /// with the specified <paramref name="dataConnection" />. If the data connection
+        /// is also associated with a transaction, it will be attached to the command.
+        /// </summary>
+        /// <param name="dataConnection">The Linq2Db data connection that is used to create the command.</param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="dataConnection" /> is null.</exception>
+        /// <exception cref="TypeCastException">Thrown when <paramref name="dataConnection" /> is not configured to use a <see cref="SqlServerProviderAdapter.SqlConnection" /> internally.</exception>
+        public static SqlCommand CreateSqlCommand(this DataConnection dataConnection)
+        {
+            dataConnection.MustNotBeNull(nameof(dataConnection));
+            var sqlConnection = dataConnection.Connection.MustBeOfType<SqlConnection>(nameof(dataConnection), $"{nameof(dataConnection)} is not configured with a {typeof(SqlConnection)}.");
+
+            var command = sqlConnection.CreateCommand();
+            if (dataConnection.Transaction != null)
+                command.Transaction = (SqlTransaction)dataConnection.Transaction;
+            return command;
+        }
+    }
+}
---
+++ Code/src/Synnotech.Linq2Db.MsSqlServer/Linq2DbSettings.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Diagnostics;
+using Light.GuardClauses;
+using Light.GuardClauses.Exceptions;
+using LinqToDB.DataProvider.SqlServer;
+using Microsoft.Extensions.Configuration;
+
+namespace Synnotech.Linq2Db.MsSqlServer
+{
+    /// <summary>
+    /// Represents the default settings for Linq2Db that are used in a setup with Microsoft SQL Server.
+    /// </summary>
+    public class Linq2DbSettings
+    {
+        /// <summary>
+        /// The default section name within the <see cref="IConfiguration" /> where settings are loaded from.
+        /// </summary>
+        public const string DefaultSectionName = "database";
+
+        /// <summary>
+        /// Gets or sets the connection string to the target database.
+        /// </summary>
+        public string ConnectionString { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Gets or sets the target version that the Linq2Db data provider will target. The default value is SqlServerVersion.v2017.
+        /// </summary>
+        public SqlServerVersion SqlServerVersion { get; set; } = SqlServerVersion.v2017;
+
+        /// <summary>
+        /// Gets or sets the value indicating whether SQL statements will be logged. The default value is Off.
+        /// </summary>
+        public TraceLevel TraceLevel { get; set; } = TraceLevel.Off;
+
+        /// <summary>
+        /// Loads the <see cref="Linq2DbSettings"/> settings from configuration.
+        /// </summary>
+        /// <param name="configuration">The configuration instance where the settings are loaded from.</param>
+        /// <param name="sectionName">The name of the section that represents the Linq2Db settings.</param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="configuration"/> or <paramref name="sectionName"/> is null.</exception>
+        /// <exception cref="ArgumentException">Thrown when <paramref name="sectionName"/> is an empty string or contains only whitespace.</exception>
+        /// <exception cref="InvalidOperationException">Thrown when the settings could not be loaded (most likely because the section is not present in the configuration).</exception>
+        public static Linq2DbSettings FromConfiguration(IConfiguration configuration, string sectionName = DefaultSectionName) =>
+            FromConfiguration<Linq2DbSettings>(configuration, sectionName);
+
+        /// <summary>
+        /// Loads the Linq2Db settings from configuration.
+        /// </summary>
+        /// <typeparam name="T">The type of Linq2Db settings that will be used to load the settings.</typeparam>
+        /// <param name="configuration">The configuration instance where the settings are loaded from.</param>
+        /// <param name="sectionName">The name of the section that represents the Linq2Db settings.</param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="configuration"/> or <paramref name="sectionName"/> is null.</exception>
+        /// <exception cref="ArgumentException">Thrown when <paramref name="sectionName"/> is an empty string or contains only whitespace.</exception>
+        /// <exception cref="InvalidOperationException">Thrown when the settings could not be loaded (most likely because the section is not present in the configuration).</exception>
+        public static T FromConfiguration<T>(IConfiguration configuration, string sectionName = DefaultSectionName)
+        {
+            configuration.MustNotBeNull(nameof(configuration));
+            sectionName.MustNotBeNullOrWhiteSpace(nameof(sectionName));
+            return configuration.GetSection(sectionName)
+                                .Get<T?>() ?? throw new InvalidConfigurationException($"Linq2Db settings could not be retrieved from configuration section \"{sectionName}\".");
+        }
+    }
+}
